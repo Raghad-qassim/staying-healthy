494cb07e5ade9814768b75593421963956a6e5bb
Raghad-qassim <raghad.qassim92@gmail.com>
2021-01-27 15:05:38 +0200

    web page update

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('welcome to our website ');
+var message='';
+
+var BMI = prompt('what is your BMI** to calculate BMI use formula is BMI = kg/m2 where kg is your weight in kilograms and m2 is your height in metres squared?');
+console.log(BMI);
+
+if (BMI<18.5){
+    message = 'Thin';
+
+}else if ( BMI <24.9){
+    message = 'Healty';
+}else if (BMI <29.9){
+    message = 'overweight';
+}else if (BMI >30 ){
+    message = 'Obese';
+
+}else{
+    message = 'calculate your BMI again'
+}
+document.write('<h3>' + message +'</h3>');
