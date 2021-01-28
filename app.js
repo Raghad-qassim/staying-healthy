750dad859597049573a7ab2c7e3d0b54bc9e3c43
Raghad-qassim <raghad.qassim92@gmail.com>
2021-01-28 18:21:59 +0200

    update

--- app.js
+++ app.js
@@ -38,11 +38,10 @@ else if(exe ==='sit-up'){
 var result =" ";
 result = img ;
 var exenum = prompt('how many time do you want to repeate the exercise?');
-for(i=0;i<exenum;i++ )
+for(i=0 ; i<exenum ; i++ )
 {
 
-    result = result + img;
-}
+    
 document.write(result);
 
-
+}
