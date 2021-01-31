31f4ec84ae5782f28b223bbdd63b3f09818737d9
Raghad-qassim <raghad.qassim92@gmail.com>
2021-01-31 16:15:37 +0200

    update

--- app.js
+++ app.js
@@ -1,46 +1,43 @@
 console.log('welcome to our website ');
-var message='';
+var message = '';
 
 var BMI = prompt('what is your BMI** to calculate BMI use formula is BMI = kg/m2 where kg is your weight in kilograms and m2 is your height in metres squared?');
 console.log(BMI);
 
-if (BMI<18.5){
+if (BMI < 18.5) {
     message = 'Thin';
 
-}else if ( BMI <24.9){
+} else if (BMI < 24.9) {
     message = 'Healty';
-}else if (BMI <29.9){
+} else if (BMI < 29.9) {
     message = 'overweight';
-}else if (BMI >30 ){
+} else if (BMI > 30) {
     message = 'Obese';
 
-}else{
+} else {
     message = 'calculate your BMI again'
 }
-document.write('<h3>' + message +'</h3>');
+document.write('<h3>' + message + '</h3>');
 
 
 var exe = prompt('what type of exercise image you want to view push-up or sit-up?');
-while(exe!=='push-up'&& exe!=='sit-up')
-{
-exe = prompt('please select the type of exercise')
+while (exe !== 'push-up' && exe !== 'sit-up') {
+    exe = prompt('please select the type of exercise')
 }
 var img = '';
-if(exe ==='push-up')
-{
+if (exe === 'push-up') {
 
-document.write(img ='<img src="push-up.jpg" />');
+    document.write(img = '<img src="push-up.jpg" />');
 
 }
-else if(exe ==='sit-up'){
-   document.write( img ='<img src="sit-up.jpg"/>');
+else if (exe === 'sit-up') {
+    document.write(img = '<img src="sit-up.jpg"/>');
 }
-var result =" ";
-result = img ;
+var result = " ";
+result = img;
 var exenum = prompt('how many time do you want to repeate the exercise?');
-for( var i=0 ;  i<=exenum ; i++ )
-{ 
-     result = result + img ;
-    document.write(result );
+for (var i = 0; i < exenum; i++) {
+    
+    document.write(img);
 
 }
